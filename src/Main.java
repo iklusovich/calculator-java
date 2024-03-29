08d1e71b0679473c5450879c04ed0eb0a0d62187
i.klusovich <ilya.klusovich.89@mail.ru>
2024-03-29 08:59:49 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         calc(scanner.nextLine());
+//        System.out.println(calc(scanner.nextLine()));
     }
 
     public static String calc(String input) throws Exception {
