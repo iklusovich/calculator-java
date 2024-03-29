7a67720e68a1b9b3e0199d3e3ee323b47b7a943f
i.klusovich <ilya.klusovich.89@mail.ru>
2024-03-29 08:58:18 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        calc(scanner.nextLine());
     }
 
     public static String calc(String input) throws Exception {
