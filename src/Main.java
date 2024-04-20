af685ec2ea6dad0d25fdacca713c0575cea482c6
i.klusovich <ilya.klusovich.89@mail.ru>
2024-04-20 14:10:24 +0300

    fox

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        calc(scanner.nextLine());
-//        System.out.println(calc(scanner.nextLine()));
+//        calc(scanner.nextLine());
+       System.out.println(calc(scanner.nextLine()));
     }
 
     public static String calc(String input) throws Exception {
